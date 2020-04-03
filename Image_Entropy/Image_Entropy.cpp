0d9aba520d4b17fddc3dac5ca232ee5589aed66b
David Wynne <dswynne16@gmail.com>
2020-04-03 17:06:52 -0400

    broke everything into functions. breaking into files now

--- Image_Entropy/Image_Entropy.cpp
+++ Image_Entropy/Image_Entropy.cpp
@@ -1,63 +1,95 @@
 // Image_Entropy.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+/* Standard Libraries: */
 #include <iostream>
 #include <string> 
 #include <vector>
 #include <stdio.h>
-#include <gmp.h>
-#include <mpir.h>
+#include <stdlib.h>
+#include <time.h>
+/* External Libraries: */
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+// MPIR
+#include <mpirxx.h>
 
 using namespace cv;
 using namespace std;
-//using mpfr::mpreal;
 
-struct retVals {
+struct bitstrings {
     std::vector <std::string> p, q;
 };
 
-/* Finds arrays of strings of p&q. 
-   Each string is 512 long.
-   This implementation weaves through the 2D matrix row by row ergo the name snake
+/* Taking the intensity values of each pixel and storing it in a 2D matrix
 */
-retVals mat_snake(int temp[][256], int rows, int cols) {
+int** find_intensity(Mat I, int rows, int cols) {
+    // intializing values
+    unsigned char intensity;
+    int** intensity_mat = 0;
+    intensity_mat = new int* [rows];
+    int i, j;
+
+    // storing intensity values
+    for (i = 0; i < rows; i++) {
+        intensity_mat[i] = new int[cols];
+        for (j = 0; j < cols; j++) {
+            intensity = I.at<uchar>(i, j);
+            intensity_mat[i][j] = int(intensity);
+        }
+    }
     
-    // converting 2D to 1D for easier manipulation
+    return intensity_mat;
+}
+
+/* Converting 2D intensity matrix to 1D intensity vector for easier manipulation
+   This implementation weaves through the 2D matrix row by row ergo the name snake.
+*/
+vector<int> mat_snake(int **intensity_mat, int rows, int cols) {
+    // intializing values
     int i, j;
     const int len = rows * cols;
     std::vector<int> allpix(len);
+
+    // storing intensity values in a 1D vector
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
-            allpix[i * cols + j] = temp[i][j];
+            allpix[i * cols + j] = intensity_mat[i][j];
         }
     }
+    return allpix;
+}
 
-    // finding arrays of p&q's
-    retVals retVals;
+/* Takes the 1D intensity vector and alternates between storing 512 long bit strings in p&q.
+   Outputting bit strings are stored in a struct of vectors.
+   Each struct vector is N long depending on the size of the image.
+*/
+bitstrings gen_bitstrings(vector<int> allpix) {
+    // intializing values
+    bitstrings bitstrings;
     string pixel;
-    int k = 0, flag = 0, ovr;
-    i = 0;
-    while (i < allpix.size()) { // (NEED TO) Throw away bit strings that are not 512 long at the end
+    int i = 0, k = 0, flag = 0, ovr;
+    
+    // finding arrays of p&q's
+    while (i < allpix.size()) {
         // checking if p[k] needs to be initialized or not
         if (flag == 1) {
-            retVals.p.push_back("");
+            bitstrings.p.push_back("");
             if (ovr - 1 == 0) { // 1 extra value
-                retVals.p[k].insert(std::end(retVals.p[k]), pixel[pixel.size() - 1]);
+                bitstrings.p[k].insert(std::end(bitstrings.p[k]), pixel[pixel.size() - 1]);
             }
             else { // 2 extra values
-                retVals.p[k].insert(std::end(retVals.p[k]), std::end(pixel) - ovr + 1, std::end(pixel));
+                bitstrings.p[k].insert(std::end(bitstrings.p[k]), std::end(pixel) - ovr + 1, std::end(pixel));
             }
             flag = 0;
         }
         else if (flag == 0) {
-            retVals.p.push_back(to_string(allpix[i]));
+            bitstrings.p.push_back(to_string(allpix[i]));
             i++;
         }
         // storing a p in a bitstring
-        while (retVals.p[k].size() < 512) {
+        while (bitstrings.p[k].size() < 512) {
             // preventing indexing outside of allpix
             if (i > allpix.size() - 1) {
                 break;
@@ -66,14 +98,14 @@ retVals mat_snake(int temp[][256], int rows, int cols) {
             pixel = to_string(allpix[i]);
             // if the current pixel will make p[k] longer than 512 take the
             // overage and put it into q[k]
-            if ((retVals.p[k].size() + pixel.size()) > 512) {
-                ovr = (retVals.p[k].size() + pixel.size()) - 512;
-                retVals.p[k].insert(std::end(retVals.p[k]), std::begin(pixel), std::end(pixel) - ovr);
+            if ((bitstrings.p[k].size() + pixel.size()) > 512) {
+                ovr = (bitstrings.p[k].size() + pixel.size()) - 512;
+                bitstrings.p[k].insert(std::end(bitstrings.p[k]), std::begin(pixel), std::end(pixel) - ovr);
                 flag = 1;
                 i++;
             }
             else {
-                retVals.p[k].insert(std::end(retVals.p[k]), std::begin(pixel), std::end(pixel));
+                bitstrings.p[k].insert(std::end(bitstrings.p[k]), std::begin(pixel), std::end(pixel));
                 i++;
             }
         }
@@ -85,21 +117,21 @@ retVals mat_snake(int temp[][256], int rows, int cols) {
 
         // checking if q[k] needs to be initialized or not
         if (flag == 1) {
-            retVals.q.push_back("");
+            bitstrings.q.push_back("");
             if (ovr - 1 == 0) { // 1 extra value
-                retVals.q[k].insert(std::end(retVals.q[k]), pixel[pixel.size() - 1]);
+                bitstrings.q[k].insert(std::end(bitstrings.q[k]), pixel[pixel.size() - 1]);
             }
             else { // 2 extra values
-                retVals.q[k].insert(std::end(retVals.q[k]), std::end(pixel) - ovr + 1, std::end(pixel));
+                bitstrings.q[k].insert(std::end(bitstrings.q[k]), std::end(pixel) - ovr + 1, std::end(pixel));
             }
             flag = 0;
         }
         else if (flag == 0) {
-            retVals.q.push_back(to_string(allpix[i]));
+            bitstrings.q.push_back(to_string(allpix[i]));
             i++;
         }
         // storing a q in a bitstring
-        while (retVals.q[k].size() < 512) {
+        while (bitstrings.q[k].size() < 512) {
             // preventing indexing outside of allpix
             if (i > allpix.size() - 1) {
                 break;
@@ -108,89 +140,72 @@ retVals mat_snake(int temp[][256], int rows, int cols) {
             pixel = to_string(allpix[i]);
             // if the current pixel will make q[k] longer than 512 take the
             // overage and put it into the next p[k]
-            if ((retVals.q[k].size() + pixel.size()) > 512) {
-                ovr = (retVals.q[k].size() + pixel.size()) - 512;
-                retVals.q[k].insert(std::end(retVals.q[k]), std::begin(pixel), std::end(pixel) - ovr);
+            if ((bitstrings.q[k].size() + pixel.size()) > 512) {
+                ovr = (bitstrings.q[k].size() + pixel.size()) - 512;
+                bitstrings.q[k].insert(std::end(bitstrings.q[k]), std::begin(pixel), std::end(pixel) - ovr);
                 flag = 1;
                 i++;
             }
             else {
-                retVals.q[k].insert(std::end(retVals.q[k]), std::begin(pixel), std::end(pixel));
+                bitstrings.q[k].insert(std::end(bitstrings.q[k]), std::begin(pixel), std::end(pixel));
                 i++;
             }
         }
         k++;
     }
     // Throwing away bit strings that aren't 512 long
-    retVals.p.erase(retVals.p.begin() + k-1);
-    retVals.q.erase(retVals.q.begin() + k-1);
-    return retVals;//check this
+    bitstrings.p.erase(bitstrings.p.begin() + k - 1);
+    bitstrings.q.erase(bitstrings.q.begin() + k - 1);
+    return bitstrings;
 }
 
-int main()
-{
-	Mat I = imread("Lena.bmp");
-    unsigned char intensity;
+/* (TODO):
+    - synchronize file paths
+    - Design at least 2 more ways to divide up the intensity matrix and develop tests for them
+    - pick source of data for choosing the bitstring that is actually used
+    - break out functions into seperate files
+    - start work on filters
+*/
+int main() {
+	// Getting image
+    Mat I = imread("Lena.bmp");
     int rows = I.rows;
     int cols = I.cols;
-    int temp[256][256];
-    int i,j;
-    // storing intensity values
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++){
-            intensity = I.at<uchar>(i, j);
-            temp[i][j] = int(intensity);
-        }
-    }
     
-    // finding arrays of p&q's as strings
+    // Storing intensity values of image
+    int** intensity_mat = find_intensity(I, rows, cols);
+        
+    // Divding up the 2D intensity matrix and storing it in a 1D intensity vector
     // (TESTING) This is one implementation of breaking up the 2D matrix the goal will be to test at least two other implementations. 
-    retVals retVals = mat_snake(temp, rows, cols);
+    vector<int> allpix = mat_snake(intensity_mat, rows, cols);
     
-    /*// This block of code was written before I knew we needed to use MIPR not GMP to work with long ints.
-    // I am leaving the logic for now on the assumption that once I get MIPR setup it will use similair logic.
-    // Since it is essentially a port of GMP to windows.
+    // Generating N p&q bitstrings from the 1D intensity vector
+    bitstrings bitstrings = gen_bitstrings(allpix);
 
-    // converting bitstrings back to ints
-    unsigned long pSZ = retVals.p.size();
-    unsigned long qSZ = retVals.q.size();
+    // (NOTE) I think we will have to do all of the mpz_t type operations inside of main and not be able to pass through functions.
+    // This probably means that we will not do too many operations on the mpz_t once it is in that form.
+    // Might just want to keep the randomization strictly image processing based
+
+    // Picking a random bitstring from the struct of vector strings for p&q
+    // (NEED TO)  Pick a source of randomization that isn't the standard rand func
+    int pSZ = bitstrings.p.size();
+    int qSZ = bitstrings.q.size();
+    srand(time(NULL));
+    int pIdx = rand() % pSZ;
+    int qIdx = rand() % qSZ;
+    std::vector<char*> cstringsP, cstringsQ; // (NEED TO) Check if these actually need to be vectors or if you can just pull it out as a const char*
+    cstringsP.reserve(pSZ);
+    cstringsP.push_back(const_cast<char*>(bitstrings.p[pIdx].c_str()));
+    cstringsQ.reserve(qSZ);
+    cstringsQ.push_back(const_cast<char*>(bitstrings.q[qIdx].c_str()));
+     
+    // Converting bitstrings back to ints
     mpz_t pInt;
     mpz_t qInt;
-    __gmpz_init2(pInt,pSZ);
-    __gmpz_init2(qInt,qSZ);
-    //std::vector<char*> str(retVals.p.size());
-    std::vector<char*> cstringsP, cstringsQ;
-    cstringsP.reserve(retVals.p.size());
-    for (i = 0; i < pSZ + 1; i++) {
-        cstringsP.push_back(const_cast<char*>(retVals.p[i].c_str()));
-        __gmpz_set_str(pInt, cstringsP[i], 10);
-    }
-    cstringsQ.reserve(retVals.q.size());
-    for (i = 0; i < qSZ + 1; i++) {
-        cstringsQ.push_back(const_cast<char*>(retVals.q[i].c_str()));
-        __gmpz_set_str(qInt, cstringsQ[i], 10);
-    }
-
-    // averaging out the p&q's (will break this into a function later)
-    mpz_t tot;
-    __gmpz_init(tot);
-    // for p
-    __gmpz_set(tot,pInt[0]);
-    for (i = 1; i < pSZ + 1; i++) {
-        __gmpz_add(tot, tot, pInt[i]);
-    }
-    mpz_t pFinal,quo;
-    __gmpz_init(pFinal);
-    __gmpz_init(quo);
-    __gmpz_set(pFinal, __gmpz_cdiv_q(quo,tot,pSZ));
-    // for q
-    __gmpz_set(tot, qInt[0]);
-    for (i = 1; i < qSZ + 1; i++) {
-        __gmpz_add(tot, tot, qInt[i]);
-    }
-    mpz_t qFinal;
-    __gmpz_init(qFinal);
-    __gmpz_set(qFinal, __gmpz_cdiv_q(quo, tot, qSZ));*/
+    mpz_init(pInt);
+    mpz_init(qInt);
+    mpz_set_str(pInt, cstringsP[0], 10);
+    mpz_set_str(qInt, cstringsQ[0], 10);
 
 
 	return 0;
